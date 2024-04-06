127b8928383c209a4a5eb6c9a5ca18f99ccc718b
Jerik-Brahmos <147366081+Jerik-Brahmos@users.noreply.github.com>
2024-04-06 23:26:00 +0530

    3rd commit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   ReactDOM.render((
-   <BrowserRouter basename={process.env.YouTube-Clone-using-React-JS/}>
-     <App />
-   </BrowserRouter>
-), ...)  
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
