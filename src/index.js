327a7513344fed154483bf3e9414919d1cb062b8
Jerik-Brahmos <147366081+Jerik-Brahmos@users.noreply.github.com>
2024-04-07 11:21:07 +0530

    4th commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
-import { HashRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter base="/">
-        <App />
-    </HashRouter>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
